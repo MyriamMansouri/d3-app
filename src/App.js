d9d54be0b02eb5824911a3e1061180ec49f453c1
myriam mansouri <myriam.mansouri@gmail.com>
2021-04-04 13:33:56 -0400

    Created a graph with d3

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 
-import './App.css';
+import React, { useEffect, useRef } from 'react';
 import * as d3 from 'd3';
 
 const App = () => {
+  const myRef = useRef(null);
+
+  const dataSet = [100, 200, 300, 400, 500];
+
+  useEffect (() => {
+    console.log(myRef);
+    d3.select(myRef.current).append('p').text('hello from d3');
+    
+    let size = 500;
+    
+    let svg = d3.select(myRef.current).append('svg').attr('width', size).attr('height', size);
+    
+    let rect_width = 200;
+
+    svg.selectAll('rect')
+    .data(dataSet)
+    .enter()
+    .append('rect')
+    .attr('x', (d, i) => 5 + i*(rect_width + 5))
+    .attr('y', d => size - d)
+    .attr('width', rect_width)
+    .attr('height', d => d)
+    .attr('fill', 'teal');
+  }, [dataSet])
+
   return (
-    <div>
+    <div ref={myRef}>
       Mon app 
     </div>
   );
